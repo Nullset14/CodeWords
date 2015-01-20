7507faffe4febefe6b1d10a5346f849f155c8e63
Ajay Davuluri <Nullset14@users.noreply.github.com>
2015-01-21 02:40:41 +0530

    Update Cooking the Books.rb

--- Hacker-Cup-2015/Cooking the Books.rb
+++ Hacker-Cup-2015/Cooking the Books.rb
@@ -1,27 +1,23 @@
+# Facebook Hacker Cup 2015 Qualification Round - Cooking the Books
 # Problem: https://www.facebook.com/hackercup/problems.php?pid=582062045257424&round=742632349177460
 # Solution: https://www.facebook.com/notes/1043281905687710/
 # ruby ~/Cooking\ the\ Books.rb < ~/cooking_the_books.txt
-#
+# 
 
-max = proc {|numbers, n| numbers.max}
-@min = proc {|numbers, n| numbers.min}
-min = proc {|numbers, n|
-  minimum = @min.call(numbers - ['0'], n)
-  raise 'MinimumError' if n[0] == minimum
-  minimum
-}
+max = proc {|numbers| numbers.max}
+min = proc {|numbers| numbers.min}
+_min = proc {|numbers| min.call(numbers - ['0'])}
 
-def cook_the_book(n, &block)
+def cook_the_book(n, condition, &block)
   return n if (n == '0' || n == '')
   numbers = n.split('')
-  begin
-    value = block.call(numbers, n)
-  rescue => e
-    raise e if e.message != 'MinimumError'
-    return (n[0] + cook_the_book(n[1..-1], &@min))
+  if block_given?
+    value = block.call(numbers)
+    return (value + cook_the_book(n[1..-1], condition)) if value == n[0]
   end
+  value = condition.call(numbers)
   if n[0] == value
-    return (value + cook_the_book(n[1..-1], &block))
+    return (value + cook_the_book(n[1..-1], condition))
   else
     index = 0
     numbers.each_with_index {|num, idx| index = idx if num == value}
@@ -35,5 +31,5 @@ $stdout.reopen('output.txt', 'w')
 STDIN.each_with_index do |line, idx|
   next if idx == 0
   n = line.chomp
-  print "Case ##{idx}: #{cook_the_book(n.dup, &min)} #{cook_the_book(n.dup, &max)}\n"
+  print "Case ##{idx}: #{cook_the_book(n.dup, min, &_min)} #{cook_the_book(n.dup, max)}\n"
 end
