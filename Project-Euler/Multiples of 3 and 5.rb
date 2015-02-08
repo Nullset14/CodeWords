cf4bb77beb12575671402a85011cbf85bbbe7617
Ajay Davuluri <Nullset14@users.noreply.github.com>
2015-02-09 01:43:30 +0530

    Create Multiples of 3 and 5.rb

---
+++ Project-Euler/Multiples of 3 and 5.rb
@@ -0,0 +1,3 @@
+# https://projecteuler.net/problem=1
+sumOfTerms = Proc.new {|multiple|  noOfTerms = 999/multiple; noOfTerms * (noOfTerms + 1) * multiple / 2}
+sumOfTerms.call(3) + sumOfTerms.call(5) - sumOfTerms.call(15)
