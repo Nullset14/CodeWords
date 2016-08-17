0367c8756da45519031d714fef7cb34157eebdfa
Ajay Kumar <Nullset14@users.noreply.github.com>
2016-08-16 23:38:09 -0400

    Update InversionCount.rb

--- InversionCount.rb
+++ InversionCount.rb
@@ -11,16 +11,14 @@ module MergeSort
     def merge_sort(arr)
       len = arr.length
       return arr if len <= 1
-      
       left = merge_sort(arr[0...len/2])
       right = merge_sort(arr[len/2...len])
-      
+     
       merge(left, right)
     end    
     
     def merge(left, right)
       result = []
-      
       until (left.empty? || right.empty?)
         result << if left.first <= right.first
                     left.shift
